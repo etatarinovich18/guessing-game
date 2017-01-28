bfc9f48114bc48bec1531fb19dbbca22c999ec23
Tatarinovich Ekaterina <etatarinovich18@gmail.com>
2017-01-28 03:00:31 +0200

    guessing-game: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.middle = Math.round((this.min + this.max)/2);
+    	
+    	return this.middle;
     }
 
     lower() {
-
+    	this.max = this.middle;
     }
 
     greater() {
-
+    	this.min = this.middle;
     }
 }
 
